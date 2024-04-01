1675a28468f7a62cd624fc3c23ac576e7d5255d7
Alona <alona.costa2@gmail.com>
2024-04-01 17:28:43 +0100

    Exercise 17

--- javascript/exercicios11-20.js
+++ javascript/exercicios11-20.js
@@ -1,3 +1,5 @@
+const { pathToArray } = require("graphql/jsutils/Path");
+
 // Exercício 11
 function exercicio11() {
   let num = document.getElementById("numero-11").value;
@@ -272,4 +274,47 @@ function exercicio16() {
 function exercicio16a() {
   document.getElementById("notaError-16").innerHTML = "";
   document.getElementById("nota-16").style.borderColor = "#2b2f78";
+}
+
+// Exercício 17
+function exercicio17() {
+  let frase = document.getElementById("frase-17").value;
+  let formOk = true;
+
+  console.log(frase);
+
+  if (frase.length < 10) {
+    document.getElementById("fraseError-17").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza no mínimo 10 caracteres!</span>';
+    formOk = false;
+    document.getElementById("frase-17").style.borderColor = "#d2161e";
+  }
+
+  //console.log(frase.split("não"));
+  //console.log(frase.split("nao"));
+  if (formOk) {
+    if (frase.includes("não")) {
+      let array = frase.split("não");
+      //array = frase.split("nao");
+      document.getElementById("textExer17").innerHTML = array;
+      document.getElementById("textExer17").style.marginTop = "15px";
+    } else if (frase.includes("nao")) {
+      let array = frase.split("nao");
+      //array = frase.split("nao");
+      document.getElementById("textExer17").innerHTML = array;
+      document.getElementById("textExer17").style.marginTop = "15px";
+    } else {
+      document.getElementById("textExer17").innerHTML = frase;
+      document.getElementById("textExer17").style.marginTop = "15px";
+    }
+  }
+}
+
+// Limpar erros na frase
+function exercicio17a() {
+  let frase = document.getElementById("frase-17").value;
+  if (frase.length >= 10) {
+    document.getElementById("fraseError-17").innerHTML = " ";
+    document.getElementById("frase-17").style.borderColor = "#2b2f78";
+  }
 }
