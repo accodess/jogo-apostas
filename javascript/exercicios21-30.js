46774c4b30e4a71decafb7dffe226fad124edc79
Alona <alona.costa2@gmail.com>
2024-04-03 17:49:20 +0100

    Exercise 22

--- javascript/exercicios21-30.js
+++ javascript/exercicios21-30.js
@@ -32,7 +32,70 @@ function exercicio21a() {
   document.getElementById("numero-21").style.borderColor = "#2b2f78";
 }
 
-// Transformar euro para dollars
+// Transformar número positivo paara o número negativo
 function converterPositParaNega(numero) {
   return numero * -1;
+}
+
+// Exercício 22
+function exercicio22() {
+  let numero = document.getElementById("numero-22").value;
+  let percent = document.getElementById("percent-22").value;
+
+  let formOk = true;
+
+  if (isNaN(numero) || numero == "") {
+    document.getElementById("numeroError-22").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o valor!</span>';
+    formOk = false;
+    document.getElementById("numero-22").style.borderColor = "#d2161e";
+  }
+
+  if (numero <= 0) {
+    document.getElementById("numeroError-22").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o valor positivo!</span>';
+    formOk = false;
+    document.getElementById("numero-22").style.borderColor = "#d2161e";
+  }
+
+  if (isNaN(percent) || percent == "") {
+    document.getElementById("percentError-22").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o percentagem do imposto!</span>';
+    formOk = false;
+    document.getElementById("percent-22").style.borderColor = "#d2161e";
+  }
+
+  if (percent <= 0) {
+    document.getElementById("percentError-22").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o percentagem positivo!</span>';
+    formOk = false;
+    document.getElementById("percent-22").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    document.getElementById("textExer22").innerHTML = devolverValorSemImpostos(
+      numero,
+      percent
+    );
+    document.getElementById("textExer22").style.margin = "10px 10px 5px 12px";
+  }
+}
+// Limpar erros no euro
+function exercicio22a() {
+  document.getElementById("numeroError-22").innerHTML = " ";
+  document.getElementById("numero-22").style.borderColor = "#2b2f78";
+}
+// Limpar erros no euro
+function exercicio22b() {
+  document.getElementById("percentError-22").innerHTML = " ";
+  document.getElementById("percent-22").style.borderColor = "#2b2f78";
+}
+
+// Devolver valor sem impostos e valor do imposto
+function devolverValorSemImpostos(valor, percentImposto) {
+  let valorImposto = (valor * percentImposto) / 100;
+  let valorSemImposto = valor - valorImposto;
+
+  return `O valor sem impostos - ${valorSemImposto} € <br>
+  O valor do imposto - ${valorImposto} €`;
 }
