6afc447652d4625143ce49366dac03d8b998601c
Alona <alona.costa2@gmail.com>
2024-04-05 17:04:49 +0100

    Exercise 23

--- javascript/exercicios21-30.js
+++ javascript/exercicios21-30.js
@@ -93,9 +93,74 @@ function exercicio22b() {
 
 // Devolver valor sem impostos e valor do imposto
 function devolverValorSemImpostos(valor, percentImposto) {
-  let valorImposto = (valor * percentImposto) / 100;
-  let valorSemImposto = valor - valorImposto;
+  let valorSemImposto = (valor / (1 + percentImposto / 100)).toFixed(2);
+  let valorImposto = (valor - valorSemImposto).toFixed(2);
 
   return `O valor sem impostos - ${valorSemImposto} € <br>
+  O valor do imposto - ${valorImposto} €`;
+}
+
+// Exercício 23
+function exercicio23() {
+  let numero = parseFloat(document.getElementById("numero-23").value);
+  let percent = parseFloat(document.getElementById("percent-23").value);
+
+  let formOk = true;
+
+  if (isNaN(numero) || numero == "") {
+    document.getElementById("numeroError-23").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o valor!</span>';
+    formOk = false;
+    document.getElementById("numero-23").style.borderColor = "#d2161e";
+  }
+
+  if (numero <= 0) {
+    document.getElementById("numeroError-23").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o valor positivo!</span>';
+    formOk = false;
+    document.getElementById("numero-23").style.borderColor = "#d2161e";
+  }
+
+  if (isNaN(percent) || percent == "") {
+    document.getElementById("percentError-23").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o percentagem do imposto!</span>';
+    formOk = false;
+    document.getElementById("percent-23").style.borderColor = "#d2161e";
+  }
+
+  if (percent <= 0) {
+    document.getElementById("percentError-23").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o percentagem positivo!</span>';
+    formOk = false;
+    document.getElementById("percent-23").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    document.getElementById("textExer23").innerHTML = devolverValorComImpostos(
+      numero,
+      percent
+    );
+    document.getElementById("textExer23").style.margin = "10px 10px 5px 12px";
+  }
+}
+// Limpar erros no euro
+function exercicio23a() {
+  document.getElementById("numeroError-23").innerHTML = " ";
+  document.getElementById("numero-23").style.borderColor = "#2b2f78";
+}
+// Limpar erros no euro
+function exercicio23b() {
+  document.getElementById("percentError-23").innerHTML = " ";
+  document.getElementById("percent-23").style.borderColor = "#2b2f78";
+}
+
+// Devolver valor sem impostos e valor do imposto
+function devolverValorComImpostos(valor, percentImposto) {
+  let valorImposto = (valor * (percentImposto / 100)).toFixed(2);
+  console.log(parseFloat(valor + valorImposto));
+  let valorComImposto = (parseFloat(valor) + parseFloat(valorImposto)).toFixed(
+    2
+  );
+  return `O valor com impostos - ${valorComImposto} € <br>
   O valor do imposto - ${valorImposto} €`;
 }
