43b754aaab54b09653b48439fd121a88143898f4
Alona <alona.costa2@gmail.com>
2024-04-19 16:29:56 +0100

    Exercisev34

--- javascript/exercicios21-30.js
+++ javascript/exercicios21-30.js
@@ -509,3 +509,44 @@ function exercicio33() {
   context.fillStyle = "#d2161e";
   context.fill();
 }
+
+// Exercício 34
+function exercicio34() {
+  let email = document.getElementById("email-34").value;
+
+  let formOk = true;
+  console.log(email.length);
+  if (email.length == 0) {
+    document.getElementById("emailError-34").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Insira o email!</span>';
+    formOk = false;
+    document.getElementById("email-34").style.borderColor = "#d2161e";
+  } else if (!email.includes("@")) {
+    document.getElementById("emailError-34").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é e-mail válido!</span>';
+    formOk = false;
+    document.getElementById("email-34").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    document.getElementById("textExer34").innerHTML = mostrarEmailEscondido();
+  }
+}
+
+// Limpar erros no número
+function exercicio34a() {
+  document.getElementById("emailError-34").innerHTML = " ";
+  document.getElementById("email-34").style.borderColor = "#2b2f78";
+}
+
+// Mostrar a parte escondida do email
+function mostrarEmailEscondido() {
+  let email = document.getElementById("email-34").value;
+  let metade = email.indexOf("@");
+  let parteMetade = Math.floor(metade / 2);
+  let metadeEscondida =
+    email.substring(0, parteMetade) +
+    "*".repeat(metade - parteMetade) +
+    email.substring(metade, email.length);
+  return metadeEscondida;
+}
