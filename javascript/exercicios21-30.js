632dde89f757be1ec101a00ed12e2b7af4da0396
Alona <alona.costa2@gmail.com>
2024-04-22 10:20:26 +0100

    Exercisev36

--- javascript/exercicios21-30.js
+++ javascript/exercicios21-30.js
@@ -598,3 +598,44 @@ function mostrarTexto() {
   <li>Todos as letras do nome separados por espaços &rarr; ${letterSeparated}</li>
   </ul>`;
 }
+
+// Exercício 36
+function exercicio36() {
+  let numero = document.getElementById("number-36").value;
+
+  let formOk = true;
+
+  if (isNaN(numero)) {
+    document.getElementById("numberError-36").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é número, digita novamente!</span>';
+    formOk = false;
+    document.getElementById("number-36").style.borderColor = "#d2161e";
+  }
+  if (numero == "") {
+    document.getElementById("numberError-36").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o número!</span>';
+    formOk = false;
+    document.getElementById("number-36").style.borderColor = "#d2161e";
+  }
+
+  let estrelas = "";
+  if (formOk) {
+    for (let i = 0; i < numero; i++) {
+      estrelas += "*";
+    }
+    let aux = "";
+    for (let i = 0; i < estrelas.length; i++) {
+      aux += "<br>";
+      for (let j = 0; j <= i; j++) {
+        aux += estrelas[j];
+      }
+    }
+    document.getElementById("textExer36").innerHTML = aux;
+    document.getElementById("textExer36").style.marginTop = "-15px";
+  }
+}
+// Limpar erros no número
+function exercicio36a() {
+  document.getElementById("numberError-36").innerHTML = " ";
+  document.getElementById("number-36").style.borderColor = "#2b2f78";
+}
