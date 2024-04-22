42aaf2df90ba547536629f929bc728f50a25ad92
Alona <alona.costa2@gmail.com>
2024-04-22 11:04:21 +0100

    Exercise 38

--- javascript/exercicios21-30.js
+++ javascript/exercicios21-30.js
@@ -639,3 +639,47 @@ function exercicio36a() {
   document.getElementById("numberError-36").innerHTML = " ";
   document.getElementById("number-36").style.borderColor = "#2b2f78";
 }
+
+// Exercício 37
+function exercicio37() {
+  let numero = document.getElementById("number-37").value;
+
+  let formOk = true;
+
+  if (isNaN(numero)) {
+    document.getElementById("numberError-37").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é número, digita novamente!</span>';
+    formOk = false;
+    document.getElementById("number-37").style.borderColor = "#d2161e";
+  }
+  if (numero == "") {
+    document.getElementById("numberError-37").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o número!</span>';
+    formOk = false;
+    document.getElementById("number-37").style.borderColor = "#d2161e";
+  }
+  let soma = 0;
+  if (formOk) {
+    for (let i = 1; i <= numero; i++) {
+      soma += i;
+      console.log(soma);
+    }
+  }
+  document.getElementById("textExer37").innerHTML = soma;
+}
+
+// Limpar erros no número
+function exercicio37a() {
+  document.getElementById("numberError-37").innerHTML = " ";
+  document.getElementById("number-37").style.borderColor = "#2b2f78";
+}
+
+// Exercício 38
+function exercicio38() {
+  let multiplosPor3 = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      document.getElementById("textExer38").innerHTML += i + " ";
+    }
+  }
+}
