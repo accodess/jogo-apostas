378c5169acb16e33f9ecde62ff9eb2c90c8defe5
Alona <alona.costa2@gmail.com>
2024-05-13 16:23:44 +0100

    Exercise 48

--- javascript/exercicios41-60.js
+++ javascript/exercicios41-60.js
@@ -359,3 +359,53 @@ function exercicio47() {
     "textExer47"
   ).innerHTML = `Números que occore mais vezes: ${mostFrequentNumber}`;
 }
+
+// Exercício 48
+function exercicio48() {
+  let numero = document.getElementById("numero-48").value;
+  let formOk = true;
+  let resultado = "";
+  console.log(numero);
+  // Verificamos introdução do preço
+  if (isNaN(numero)) {
+    document.getElementById("numeroError-48").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é número, digita novamente!</span>';
+    formOk = false;
+    document.getElementById("numero-48").style.borderColor = "#d2161e";
+  }
+  if (numero == "") {
+    document.getElementById("numeroError-48").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o número!</span>';
+    formOk = false;
+    document.getElementById("numero-48").style.borderColor = "#d2161e";
+  } else if (numero.length < 4) {
+    document.getElementById("numeroError-48").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o número maior que 1000!</span>';
+    formOk = false;
+    document.getElementById("numero-48").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    console.log(numero.length);
+    for (let i = 0; i < numero.length; i++) {
+      console.log(resultado);
+      if (
+        i < numero.length - 1 &&
+        parseInt(numero[i]) % 2 == 0 &&
+        parseInt(numero[i + 1]) % 2 == 0
+      ) {
+        resultado += numero[i] + "-";
+        console.log(numero[i]);
+      } else {
+        resultado += numero[i];
+      }
+    }
+    document.getElementById("textExer48").innerHTML = resultado;
+  }
+}
+
+// Limpar erros no número
+function exercicio48a() {
+  document.getElementById("numeroError-48").innerHTML = " ";
+  document.getElementById("numero-48").style.borderColor = "#2b2f78";
+}
