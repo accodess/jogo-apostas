75343eb66e58a2052faac50dbc8d7747fc10e20b
Alona <alona.costa2@gmail.com>
2024-05-15 11:43:52 +0100

    Exercise 52

--- javascript/exercicios41-60.js
+++ javascript/exercicios41-60.js
@@ -505,3 +505,37 @@ function exercicio51a() {
   document.getElementById("fraseError-51").innerHTML = " ";
   document.getElementById("frase-51").style.borderColor = "#2b2f78";
 }
+
+// Exercício 52
+function exercicio52() {
+  let numero = parseInt(document.getElementById("numero-52").value);
+  let formOk = true;
+  let resultado = "";
+
+  // Verificamos introdução do número
+  if (isNaN(numero)) {
+    document.getElementById("numeroError-52").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é número, digita novamente!</span>';
+    formOk = false;
+    document.getElementById("numero-52").style.borderColor = "#d2161e";
+  }
+  if (numero == "") {
+    document.getElementById("numeroError-52").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza o número!</span>';
+    formOk = false;
+    document.getElementById("numero-52").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    resultado = (numero >>> 0).toString(2);
+    document.getElementById(
+      "textExer52"
+    ).innerHTML = `Número decimal: &nbsp; ${numero} <br> Número binário: &nbsp; ${resultado}`;
+  }
+}
+
+// Limpar erros no número
+function exercicio52a() {
+  document.getElementById("numeroError-52").innerHTML = " ";
+  document.getElementById("numero-52").style.borderColor = "#2b2f78";
+}
