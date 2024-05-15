88d92ce92d435dc096f78dc454bb04d518456723
Alona <alona.costa2@gmail.com>
2024-05-15 11:26:39 +0100

    Exercise 50 corrections, Exercise 51

--- javascript/exercicios41-60.js
+++ javascript/exercicios41-60.js
@@ -365,8 +365,8 @@ function exercicio48() {
   let numero = document.getElementById("numero-48").value;
   let formOk = true;
   let resultado = "";
-  console.log(numero);
-  // Verificamos introdução do preço
+
+  // Verificamos introdução do número
   if (isNaN(numero)) {
     document.getElementById("numeroError-48").innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Não é número, digita novamente!</span>';
@@ -445,6 +445,7 @@ function exercicio50() {
   let resultado = "";
 
   let tabela = document.getElementById("matrizTabela50");
+  tabela.style.visibility = "visible";
   tabela.innerHTML = "";
 
   for (let i = 0; i < 10; i++) {
@@ -476,5 +477,31 @@ function exercicio50() {
   document.getElementById(
     "textExer50"
   ).innerHTML = `Quantidade de elementos entre 10 e 20 &rarr; ${quantidade} números.`;
-  document.getElementById("textExer50").style.marginTop = "10px";
+  document.getElementById("textExer50").style.marginTop = "15px";
+}
+
+// Exercício 51
+function exercicio51() {
+  let frase = document.getElementById("frase-51").value;
+  let formOk = true;
+  let resultado = "";
+
+  // Verificamos introdução da frase
+  if (frase == "") {
+    document.getElementById("fraseError-51").innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512" height="512" class="error-img"><g><path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0S0,114.615,0,256C0.153,397.322,114.678,511.847,256,512z    M234.667,128c0-11.782,9.551-21.333,21.333-21.333c11.782,0,21.333,9.551,21.333,21.333v170.667   c0,11.782-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333V128z M256,384c11.782,0,21.333,9.551,21.333,21.333   s-9.551,21.333-21.333,21.333c-11.782,0-21.333-9.551-21.333-21.333S244.218,384,256,384z"/></g><span class="error-text-4"> Por favor introduza a palavra-passe!</span>';
+    formOk = false;
+    document.getElementById("frase-51").style.borderColor = "#d2161e";
+  }
+
+  if (formOk) {
+    resultado = frase.replace(/[^\w\s]/gi, "");
+    document.getElementById("textExer51").innerHTML = resultado;
+  }
+}
+
+// Limpar erros no número
+function exercicio51a() {
+  document.getElementById("fraseError-51").innerHTML = " ";
+  document.getElementById("frase-51").style.borderColor = "#2b2f78";
 }
